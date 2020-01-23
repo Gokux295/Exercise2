3e9906712f111c8915d2831bb0ccfd0dcc5c572f
tatur24 <54002798+tatur24@users.noreply.github.com>
2020-01-22 20:34:07 -0600

    Add files via upload

---
+++ excercise2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class prog3{
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+
+        
+        //String name="Chad";
+
+        System.out.println("Welcome " + name);
+
+        //variables DataType NameVariable = value;
+
+float salary = 80000;
+Integer Age = 35;
+
+
+System.out.println("Your Salary is " + salary);
+System.out.println("Your age is " + Age);
+
+
+
+    }
+}
