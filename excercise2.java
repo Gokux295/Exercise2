6fe00f530a944ada227b0ee14ea0ad07dcf220f6
tatur24 <54002798+tatur24@users.noreply.github.com>
2020-01-24 19:49:50 -0600

    Update excercise2.java

--- excercise2.java
+++ excercise2.java
@@ -1,27 +1,9 @@
-import java.util.Scanner;
-
-public class prog3{
-    public static void main(String args[]){
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = sc.nextLine();
-
-        
-        //String name="Chad";
-
-        System.out.println("Welcome " + name);
-
-        //variables DataType NameVariable = value;
-
-float salary = 80000;
-Integer Age = 35;
-
-
-System.out.println("Your Salary is " + salary);
-System.out.println("Your age is " + Age);
-
-
-
+public class exercise2{
+       public static void main(String args[]){
+        float salary = 80000;
+        Integer Age = 35;
+        System.out.println("Chad");
+        System.out.println("Your age is " + Age);
+        System.out.println("Your Salary is " + salary);
     }
-}
+    }
